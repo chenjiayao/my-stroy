03417bd869fda41befcead91c83f4e12e398ec6f
chenjiayao <chenjiayaooo@gmail.com>
2022-05-05 11:38:03 +0800

    use vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    theme: 'reco',
+    themeConfig: {
+        type: "blog",
+        nav: [
+            { text: '首页', link: '/' },
+            {
+                text: 'JayChen\'s story', link: 'https://blog.jaychen.fun'
+            },
+            {
+                text: 'Github', link: 'https://github.com/chenjiayao'
+            }
+        ],
+        subSidebar: 'auto',
+        sidebar: [
+            { title: "首页", path: "/" },
+        ]
+    }
+}  
