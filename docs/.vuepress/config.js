50c98a310ec5a4755d7ff0b04a10d715eae9ee37
chenjiayao <chenjiayaooo@gmail.com>
2022-05-06 22:25:34 +0800

    vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,19 +1,22 @@
 module.exports = {
     theme: 'reco',
     themeConfig: {
-        type: "blog",
+        lastUpdated: 'Last Updated', // string | boolean
+        blogConfig: {
+            category: {
+                location: 2,     // 在导航栏菜单中所占的位置，默认2
+                text: 'Category' // 默认文案 “分类”
+            }
+        },
+        subSidebar: 'auto',
+
         nav: [
-            { text: '首页', link: '/' },
-            {
-                text: 'JayChen\'s story', link: 'https://blog.jaychen.fun'
-            },
+            { text: '首页', link: '/' ,icon:'reco-home'},
+
+            { text: '时间线', link: '/timeline/', icon: 'reco-date' },
             {
                 text: 'Github', link: 'https://github.com/chenjiayao'
             }
-        ],
-        subSidebar: 'auto',
-        sidebar: [
-            { title: "首页", path: "/" },
         ]
     }
 }  
